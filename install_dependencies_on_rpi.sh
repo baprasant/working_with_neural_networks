bfc7458c193391b832ef18c7e4d75ec1e7308168
Prasanth <pbhamidipati@duncansolutions.com.au>
2020-08-19 16:02:06 +1000

    updated the bash script to install dependencies on r-pi

--- install_dependencies_on_rpi.sh
+++ install_dependencies_on_rpi.sh
@@ -16,3 +16,11 @@ pip3 install https://github.com/lhelontra/tensorflow-on-arm/releases/download/v2
 # Checking Installation
 python3 -c "import tensorflow as tf;print(tf.__version__)"
 # Should be getting 2.1.0
+
+pip3 install pandas
+pip3 install keras==2.3.1
+pip3 install matplotlib
+
+
+sudo apt-get install libopenjp2-7
+sudo apt-get install libtiff5
